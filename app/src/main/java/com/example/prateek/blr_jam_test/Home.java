0a7afa13ebce553c7a2b42ff002dc592d52c01e2
Prateek Agarwal <prateek.agar@gmail.com>
2016-04-30 15:02:38 +0530

    updated with text view in java file

--- app/src/main/java/com/example/prateek/blr_jam_test/Home.java
+++ app/src/main/java/com/example/prateek/blr_jam_test/Home.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
@@ -17,6 +18,9 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView textView = (TextView) findViewById(R.id.demo);
+        textView.setText("Some text");
 
     }
 
